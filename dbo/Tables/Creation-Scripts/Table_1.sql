61bd1d2c888be972ab06bfc2c6db61b9ca1e23ce
roylauzon <email>
2022-01-30 13:49:49 +0000

    changes to table_1

--- dbo/Tables/Creation-Scripts/Table_1.sql
+++ dbo/Tables/Creation-Scripts/Table_1.sql
@@ -3,7 +3,8 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Table_1](
-	[id1] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+	[id1] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[id2_2] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 )
 
 GO
